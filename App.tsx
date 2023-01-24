0b42bd8b771d2d4c185b73e1915c97a3b8cfad52
Владислав Ломака <vladislavlomaka@MacBook-Pro-Vladislav.local>
2023-01-24 14:13:29 +0200

    gradient background added

--- App.tsx
+++ App.tsx
@@ -1,18 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <LinearGradient colors={['#ff009d','white', 'red']} style={{height: '100%', width: '100%', flex: 1}}>
+    </LinearGradient>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
