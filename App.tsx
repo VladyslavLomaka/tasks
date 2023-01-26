dc4e9bfb24b8ca05ac8192b9e0d364b984221932
Владислав Ломака <vladislavlomaka@MacBook-Pro-Vladislav.local>
2023-01-26 10:40:39 +0200

    flash-massage

--- App.tsx
+++ App.tsx
@@ -1,9 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import FlashMessage, { showMessage } from 'react-native-flash-message';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <TouchableOpacity
+        onPress={() => {
+          showMessage({
+            message: 'Boom!',
+            description: 'You got it!',
+            type: 'success',
+          });
+        }}>
+        <Text style={{ fontSize: 24, letterSpacing: 6 }}>Splash!</Text>
+      </TouchableOpacity>
+
+      <FlashMessage
+        position='center'
+        style={{ width: '50%', alignItems: 'center' }}
+      />
     </View>
   );
 }
