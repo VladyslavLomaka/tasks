6dfc454166aeefbf5740c5902b67b6a88c77870f
Владислав Ломака <vladislavlomaka@MacBook-Pro-Vladislav.local>
2023-01-27 17:18:53 +0200

    get phone brand

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
+
+let brand = DeviceInfo.getBrand();
+
 
 export default function App() {
+  console.log(brand)
+
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello World! it's {brand}</Text>
     </View>
   );
 }
